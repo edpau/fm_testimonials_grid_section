cb0088c02dc90a917f62a1ff899edbd598160147
Ed <onikage116@gmail.com>
2024-09-11 23:11:09 +0100

    chore: initial project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "brand-primary": "hsl(210, 46%, 95%)",
+        "testimonialOne": "hsl(263, 55%, 52%)",
+        "testimonialTwo": "hsl(217, 19%, 35%)",
+        "testimonialThree": "hsl(0, 0%, 100%)",
+        "testimonialFour": " hsl(219, 29%, 14%)",
+        "testimonialFive": "hsl(0, 0%, 100%)",
+      },
+      textColor: {
+        "light-primary": "hsl(0, 0%, 100%)",
+        "dark-primary": "hsl(217, 19%, 35%)",
+      },
+      boxShadow:{
+        testimonial: "40px 60px 50px -47px rgba(72, 85, 106, 0.25)",
+      },
+      fontSize: {
+        paragraph: "0.8125rem",
+      },
+      fontFamily: {
+        card: ["Barlow Semi Condensed", "sans-serif"],
+      },
+      screen: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+    },
   },
   plugins: [],
 }
