60719914718c4e04bb70fb42dc2565efe9b330e5
edpau <edpau@me.com>
2024-09-25 12:03:12 +0100

    feat: add accessible h1 element to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import TestimonialContainer from "./components/TestimonialContainer";
 
 const App = () => {
   return <div className="min-h-screen bg-brand-primary">
+    <h1 className="sr-only">Testimonials Grid Section</h1>
     <TestimonialContainer />
   </div>;
 };
